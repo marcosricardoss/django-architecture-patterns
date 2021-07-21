7efe47879699837b9de3e24bce517b253c19703c
Marcos Ricardo <marcosricardoss@gmail.com>
2021-07-21 17:14:01 -0300

    test: update test_task_create.py

--- todo/apps/task/tests/e2e/test_task_create.py
+++ todo/apps/task/tests/e2e/test_task_create.py
@@ -54,7 +54,6 @@ def test_post_task_via_form_with_invalid_data(client, dates):
     assert response.content.count(b"CREATE TASK") == 1
 
 
-@pytest.mark.skip
 @pytest.mark.django_db
 def test_task_create_view_page_when_do_click_on_save_button(browser):
     browser.get(f"{APP_URL}/create/")
